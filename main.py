6ba45936b74f63d49e845729fbdc0d97870dc4bc
Weagook <weagook@gmail.com>
2022-11-11 15:11:51 +0600

    Added a universal function for finding files both in .exe format and in .py

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 import pyautogui as pg
 import webbrowser as wb
 from os import path
+import sys
 
+
+
+def resourse_path(relative_path):
+    try:
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = path.abspath('.')
+    
+    return path.join(base_path, relative_path)
 
 def start() -> None: 
     pg.alert('Программа для автоматического входа на платформу\nНеобходимо после ввода логина и пароля подождать несколько секунд (8-10) и не трогать мышь и клавиатуру.', button='Перейти')
@@ -14,29 +24,29 @@ def start() -> None:
     sign_account(login, password)
 
 def exit_account() -> None:
-    log_in = pg.locateCenterOnScreen(path.abspath(path.join(path.dirname(__file__) + '\\img\\' 'swap.png')))
+    log_in = pg.locateCenterOnScreen(resourse_path('img\\swap.png'))
     if log_in:
         pg.moveTo(log_in[0], log_in[1], duration=0.5)
         pg.click()
         pg.sleep(1)
-        logout = pg.locateCenterOnScreen(path.abspath(path.join(path.dirname(__file__) + '\\img\\' 'logout.png')))
+        logout = pg.locateCenterOnScreen(resourse_path('img\\logout.png'))
         pg.moveTo(logout[0], logout[1], duration=0.5)
         pg.click()
 
 def sign_account(login, password) -> None: 
     try:
-        input_log = pg.locateCenterOnScreen(path.abspath(path.join(path.dirname(__file__) + '\\img\\' 'input_login.png')))
+        input_log = pg.locateCenterOnScreen(resourse_path('img\\input_login.png'))
         if input_log:
             pg.moveTo(input_log[0], input_log[1], duration=0.5)
             pg.click()
             pg.write(login, interval=0.2)
-            skip = pg.locateCenterOnScreen(path.abspath(path.join(path.dirname(__file__) + '\\img\\' 'next.png')))
+            skip = pg.locateCenterOnScreen(resourse_path('img\\next.png'))
             pg.moveTo(skip[0], skip[1], duration=0.2)
             pg.click()
             pg.sleep(1)
             pg.write(password, interval=0.2)
             pg.sleep(0.3)
-            sign_in = pg.locateCenterOnScreen(path.abspath(path.join(path.dirname(__file__) + '\\img\\' 'sign_in.png')))
+            sign_in = pg.locateCenterOnScreen(resourse_path('img\\sign_in.png'))
             pg.moveTo(sign_in[0], sign_in[1], duration=0.3)
             pg.click()
     except TypeError:
