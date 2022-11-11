354b558af1660462cdbb197bf0871ee081e0beb8
Weagook <weagook@gmail.com>
2022-11-11 13:11:07 +0600

    Create app

---
+++ main.py
@@ -0,0 +1,49 @@
+import pyautogui as pg
+import webbrowser as wb
+from os import path
+
+
+def start() -> None: 
+    pg.alert('Программа для автоматического входа на платформу\nНеобходимо после ввода логина и пароля подождать несколько секунд (8-10) и не трогать мышь и клавиатуру.', button='Перейти')
+    login = pg.prompt('Введите ваш логин:')
+    password = pg.prompt('Введите пароль (как правило это 4 цифры):')
+    wb.open('http://mars.algoritmika.org')
+    pg.sleep(2)
+    exit_account()
+    pg.sleep(1)
+    sign_account(login, password)
+
+def exit_account() -> None:
+    log_in = pg.locateCenterOnScreen(path.abspath(path.join(path.dirname(__file__) + '\\img\\' 'swap.png')))
+    if log_in:
+        pg.moveTo(log_in[0], log_in[1], duration=0.5)
+        pg.click()
+        pg.sleep(1)
+        logout = pg.locateCenterOnScreen(path.abspath(path.join(path.dirname(__file__) + '\\img\\' 'logout.png')))
+        pg.moveTo(logout[0], logout[1], duration=0.5)
+        pg.click()
+
+def sign_account(login, password) -> None: 
+    try:
+        input_log = pg.locateCenterOnScreen(path.abspath(path.join(path.dirname(__file__) + '\\img\\' 'input_login.png')))
+        if input_log:
+            pg.moveTo(input_log[0], input_log[1], duration=0.5)
+            pg.click()
+            pg.write(login, interval=0.2)
+            skip = pg.locateCenterOnScreen(path.abspath(path.join(path.dirname(__file__) + '\\img\\' 'next.png')))
+            pg.moveTo(skip[0], skip[1], duration=0.2)
+            pg.click()
+            pg.sleep(1)
+            pg.write(password, interval=0.2)
+            pg.sleep(0.3)
+            sign_in = pg.locateCenterOnScreen(path.abspath(path.join(path.dirname(__file__) + '\\img\\' 'sign_in.png')))
+            pg.moveTo(sign_in[0], sign_in[1], duration=0.3)
+            pg.click()
+    except TypeError:
+        pg.alert('Простите, бот не нашел одно из окошек :(')
+    finally:
+        pg.sleep(0.5)
+        pg.alert('Бот завершил свою работу.')
+
+if __name__ == '__main__':
+    start()
